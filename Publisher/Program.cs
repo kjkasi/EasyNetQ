34acbe49e12d25a4429510ba3275c680402392b4
kjkasi <5842602+kjkasi@users.noreply.github.com>
2022-11-28 20:33:47 +0300

    No message

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Publisher
+﻿using EasyNetQ;
+using Messages;
+
+namespace Publisher
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                var input = String.Empty;
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    bus.PubSub.Publish(new TextMessage { Text = input });
+                    Console.WriteLine("Message published!");
+                }
+            }
         }
     }
 }
--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Subscriber
+﻿using EasyNetQ;
+using Messages;
+
+namespace Subscriber
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.PubSub.Subscribe<TextMessage>("test", HandleTextMessage);
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
         }
     }
 }
